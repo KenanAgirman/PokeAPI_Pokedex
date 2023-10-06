216f43bbb99dfdb7dc70830e2e20da0152b9a1d2
KenanAgirman <kenan.agirman@student.hepl.be>
2023-10-06 12:26:40 +0200

    ajout des messages d'erreur

--- code.js
+++ code.js
@@ -1,22 +1,34 @@
 let image = document.getElementById("image");
 let searchPoke = document.getElementById("searchPoke");
 let submitButton = document.getElementById("submitButton");
+let errorMessage = document.getElementById("errorMessage");
 
 const TraitemenPoke = async () =>{
     console.log("La fonction TraitemenPoke a été appelée.");
-    var pokemon = searchPoke.value;
-
-    console.log(pokemon);
+    var pokemon = searchPoke.value.trim();
     
-    let request = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    if(pokemon !== "")
+    {
+        console.log(pokemon);
+        
+        let request = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
+    
+        let data = await fetch(request);
+    
+        console.log(data);
+    
+        if(data.ok)
+        {
+            let reponse = await data.json();
+            image.src = reponse.sprites.front_default; 
+        } else {
+            errorMessage.textContent =("Le pokemon n'existe pas");
+        }
 
-    let data = await fetch(request);
+    } else {
+        errorMessage.textContent =("Le champ ne peut pas être vide.");
+    }
 
-    console.log(data);
-
-    let reponse = await data.json();
-
-    image.src = reponse.sprites.front_default;
 }
 
 submitButton.addEventListener("click",TraitemenPoke);
