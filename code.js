0870ecd443c3b9faa1a08c6b42d66bb4780c3e55
KenanAgirman <kenan.agirman@student.hepl.be>
2023-11-01 18:57:34 +0100

    *

--- code.js
+++ code.js
@@ -3,30 +3,30 @@ let imageShiny = document.getElementById("imageShiny");
 let searchPoke = document.getElementById("searchPoke");
 let submitButton = document.getElementById("submitButton");
 let errorMessage = document.getElementById("errorMessage");
-let types = document.getElementById("types");
+let type = document.getElementById("types");
 
 const TraitemenPoke = async () =>{
     console.log("La fonction TraitemenPoke a été appelée.");
     var pokemon = searchPoke.value.trim();
-    
+
     if(pokemon !== "")
     {
         console.log(pokemon);
-        
+
         let request = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-    
+
         let data = await fetch(request);
-    
+
         console.log(data);
-    
+
         if(data.ok)
         {
             let reponse = await data.json();
-            imageShiny.src = reponse.sprites.front_shiny; 
+            imageShiny.src = reponse.sprites.front_shiny;
             image.src = reponse.sprites.front_default;
-            types.types = reponse.types.name;
+            type.textContent = reponse.types[0].type.name;
 
-            console.log(reponse.types.name);
+            console.log(reponse.types[0].type.name);
 
             errorMessage.textContent = " ";
         } else{
